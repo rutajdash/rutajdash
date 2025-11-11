9e741bf4f79867e7be9f7e2ad99ceabba1afc948
Rutaj Dash <33367546+rutajdash@users.noreply.github.com>
2025-11-12 02:04:54 +0530

    fix: eslint config for type checking

--- portfolio-agent/eslint.config.js
+++ portfolio-agent/eslint.config.js
@@ -4,8 +4,18 @@ import { defineConfig } from "eslint/config";
 import tseslint from "typescript-eslint";
 
 export default defineConfig([
+  {
+    ignores: ["build", "node_modules", "dist"],
+  },
   js.configs.recommended,
   tseslint.configs.strictTypeChecked,
   tseslint.configs.eslintRecommended,
   eslintConfigPrettier,
+  {
+    languageOptions: {
+      parserOptions: {
+        project: "./tsconfig.json",
+      },
+    },
+  },
 ]);
