54361bde565fd3734a66be0ebf36f5a162b8117f
Rutaj Dash <33367546+rutajdash@users.noreply.github.com>
2025-11-13 01:37:22 +0530

    feat: add initial conversation starter message

--- portfolio-agent/src/handlers/userSocket.handler.ts
+++ portfolio-agent/src/handlers/userSocket.handler.ts
@@ -15,7 +15,16 @@ export default function handleUserSocket({
   speechSocket: WebSocket;
 }): void {
   userSocket.data = {
-    history: [] as Content[],
+    history: [
+      {
+        role: "model",
+        parts: [
+          {
+            text: "Hey, I'm Pebbles! As Mr.Dash's personal assistant, I have access to his resume, portfolio and projects. I can help you with things like telling you more about him, if you want, or about his projects, how they're going and the latest updates. I may be a penguin, but I promise I'm smart. So, what can I help you with today?",
+          },
+        ],
+      },
+    ] as Content[],
   };
 
   startSpeechContext({
